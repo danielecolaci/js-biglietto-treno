f8ad645da4c08a8a3d526d676eac37c1fcb466ab
danielecolaci <danielecolaci@outlook.com>
2024-02-19 16:53:46 +0100

    Add Final price calc based on age

--- README.rd
+++ README.rd
@@ -1,4 +1,4 @@
-Descrizione:
+## Descrizione:
 Trenitalia ha bisogno di un programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
     -Il prezzo del biglietto è definito in base ai km (0.21 € al km);
--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,19 @@ console.log(age);
 //Prezzo biglietto
 let price = km * 0.21;
 console.log(price);
-alert("Il prezzo del tuo biglietto è " + price + "€")
+
+//Sconto
+let final_price;
+
+if (age < 18) {
+    final_price = (price * 0.8);
+}
+else if (age > 65) {
+    final_price = (price * 0.6);
+}
+else {
+    final_price = price;
+}
+
+console.log(final_price)
+alert("Il prezzo del tuo biglietto è " + final_price + "€")
